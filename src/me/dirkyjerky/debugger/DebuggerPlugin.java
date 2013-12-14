2ff51e5c00c36f1706c723bea341bd3c9793e7ec
Dirky fJerky <geoffreyiy1@gmail.com>
2013-12-14 00:49:59 -0600

    Actually make it work
    Tracks 4 events supposedly relating to TNT tracking to evaluate how the
    proccess is handled

--- src/me/dirkyjerky/debugger/DebuggerPlugin.java
+++ src/me/dirkyjerky/debugger/DebuggerPlugin.java
@@ -1,24 +1,53 @@
 package me.dirkyjerky.debugger;
 
+import java.util.logging.Level;
+
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.Event;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 public class DebuggerPlugin extends JavaPlugin implements Listener {
+	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
+		rEvents(this, this);
 	};
+	@Override
 	public void onDisable() {
 	};
 
 	@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=false)
-	void onEvent(Event event) {
+	public void onExplostionEventEvent(ExplosionPrimeEvent event) {
 		
-		Bukkit.getServer().broadcastMessage("Event: " + event.getEventName());
-	};
+		Bukkit.getServer().getLogger().log(Level.INFO, event.getEventName());
+		event.setCancelled(true);
 	
+	}
+	@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=false)
+	public void onEntityInteractEvent(EntityInteractEvent event) {
+		
+		Bukkit.getServer().getLogger().log(Level.INFO, event.getEventName());
 	
+	}
+	@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=false)
+	public void onEntityExplodeEvent(EntityExplodeEvent event) {
+		
+		Bukkit.getServer().getLogger().log(Level.INFO, event.getEventName());
+	
+	}
+	@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=false)
+	public void onEntityChangeBlockEvent(EntityChangeBlockEvent event) {
+		
+		Bukkit.getServer().getLogger().log(Level.INFO, event.getEventName());
+	
+	}
+	private void rEvents(Listener listener, Plugin plugin) {
+		getServer().getPluginManager().registerEvents(listener, plugin);
+	}
 }
